67d1b0f4d0710c739a222489380d5e3f6cd5c121
priscillavarrenti <priscillavarrentilds@gmail.com>
2026-03-07 21:18:03 -0300

    update 4 product.js

--- src/js/product.js
+++ src/js/product.js
@@ -5,6 +5,7 @@ const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || [];
+  
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
