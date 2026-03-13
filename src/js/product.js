287008e90ca069904ebf54765dc37a1431103220
EmmaKOkyere <okyere.ekakra@gmail.com>
2026-03-13 17:30:51 +0000

    Dynamic Product Detail Page

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,16 @@
 // We bring in 'helpers' from other files.
 // utils.mjs' handles the browser's storage,
 // and 'ProductData.mjs' handles fetching our tent data.
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getParam, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+const productId = getParam("product");
+
+console.log(productId);
+
+console.log(dataSource.findProductById(productId));
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
   cartItems.push(product);
