27bad3b7d6435808ce0031bb25a2fbf20002c2d2
Crispus-256 <nuweamanyacrispusinfinityeneri@gmail.com>
2026-03-16 18:12:01 +0300

    individual activity

--- src/js/product.js
+++ src/js/product.js
@@ -8,7 +8,6 @@ const productID = getParam("product");
 const product = new ProductDetails(productID, dataSource);
 product.init();
 
-
 // function addProductToCart(product) {
 //   const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
 //   cartItems.push(product);
